74961318feb2b9167cdb376edff5eeb403d01b1c
Javier <yetator@gmail.com>
2016-09-30 20:53:40 +0000

    asosiando productos y categorias

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -69,6 +69,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:cod_product, :nom_product, :estado)
+      params.require(:product).permit(:cod_product, :nom_product, :estado, :category_id)
     end
 end
