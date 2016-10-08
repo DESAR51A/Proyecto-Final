ad82445b7bd9e1f716edf97e6cfdd043bdb8835f
Javier <yetator@gmail.com>
2016-10-08 23:40:29 +0000

    haciendo merge

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20161008202514) do
-ActiveRecord::Schema.define(version: 20161008201535) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",       limit: 255
