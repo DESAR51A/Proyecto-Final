7cedc2013f4ecc5a3b0d97512662d99b031bc180
Javier <yetator@gmail.com>
2016-10-11 05:12:51 +0000

    rake db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161011033350) do
+ActiveRecord::Schema.define(version: 20161011041150) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",       limit: 255
